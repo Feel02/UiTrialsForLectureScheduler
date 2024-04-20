0d99968efa2040722e9e47c6ccb1d1e132787524
Enes Sapan <enessapan0@gmail.com>
2024-04-20 12:10:24 +0300

    test6

--- js/withname.js
+++ js/withname.js
@@ -4,7 +4,8 @@ import {
     TableTemplate,
 } from "../templates/templates.js";
 
-const html2pdf = require('html2pdf.js');
+import {html2pdf} from 'html2pdf.js';
+
 const btn = document.getElementById("button1111");
 
 btn.addEventListener("click", function(){
