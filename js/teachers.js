0177222218ac570d4271a8c94e9b7e16f2b753a3
Enes Sapan <enessapan0@gmail.com>
2024-04-21 12:01:43 +0300

    test46

--- js/teachers.js
+++ js/teachers.js
@@ -6,8 +6,7 @@ const btn = document.getElementById("button1112");
 
 btn.addEventListener("click", function(){
     console.log("clicked");
-    var element = document.getElementById('tt');                  //tt for only the tables         
-    element.classList.add('overflow-cell');  
+    var element = document.getElementById('tt');                  //tt for only the tables           
 
     var opt = {
         margin:       1,
