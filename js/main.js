eb53e618bf022d91bef67d7ad8f97115de5a771a
Enes Sapan <enessapan0@gmail.com>
2024-05-11 16:29:10 +0300

    test 145

--- js/main.js
+++ js/main.js
@@ -7,7 +7,7 @@ import {
 import "https://cdnjs.cloudflare.com/ajax/libs/html2pdf.js/0.10.1/html2pdf.bundle.min.js";
 import "https://cdnjs.cloudflare.com/ajax/libs/xlsx/0.18.0/xlsx.full.min.js";
 
-const days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
+var days = ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"];
 
 const btn = document.getElementById("button1114");
 const btn2 = document.getElementById("button1124");
